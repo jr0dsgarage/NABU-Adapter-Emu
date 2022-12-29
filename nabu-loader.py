2e1889f843b24feb776728c64c4dee0f02f27a6d
Sark2338 <121634424+Sark2338@users.noreply.github.com>
2022-12-29 18:08:46 +0000

    Add files via upload
    First submission. Lots of changes and additions to original code

---
+++ nabu-loader.py
@@ -0,0 +1,315 @@
+#!/usr/bin/env python3
+# 
+# Original code: NABU Adaptor Emulator - Copyright Mike Debreceni - 2022
+#       https://github.com/mdebreceni/nabu-pc-playground/
+#   Source of bulk of code to load, parse and send segment data to the NABU and handle requests
+#
+# Major rewrite/expansion by Sark, 12/28/2022:
+# 
+#   Updated to work with original unmodified (but decrypted) cycle files from NABU network.
+#   Added support for loading from a directory of pak files
+#   Added time segment generation
+#   Added checksum generation and segment checking/patching
+#   Added trailing 1a detection and patching
+#   Terminology fixup to update variable names, notes and messages:
+#       Segment is a chunk of a program, less than 1K in size
+#       Pak is a group of segments
+#   Added checking/recovery for files that don't exist: when in doubt... penguins!
+#   Pak files are only loaded from disk once as needed, then stored in library variable
+#   Output messages trimmed and condensed
+#   Pak directory selectable from command line option, but defaults available for all settings
+
+# This works with a directory of pak files, specified at command line or by default variable below
+# Will work with unmodified (but decrypted) files from both cycle 1 and 2 of the original NABU network
+# Filename 000001.pak is menu and is loaded to begin, the rest are uppercase hex filenames with .pak extension
+# TODO: implement pak file decryption and Internet loading from nabu cloud
+
+import serial
+import time
+import os
+import argparse
+
+from nabu_data import NabuSegment, NabuPak #, NabuTimeSegment
+
+def send_ack():
+    sendBytes(bytes([0x10, 0x06]))
+
+def send_time():
+##  Pre-formed time segment, sends Jan 1 1984 at 00:00:00
+##    sendBytes(bytes([0x7f, 0xff, 0xff, 0x00, 0x01, 0x7f, 0xff, 0xff, 0xff, 0x7f, 0x80, 0x20, 0x00, 0x00, 0x00, 0x00, 0x02, 0x02, 0x02, 0x54, 0x01, 0x01, 0x00, 0x00, 0x00, 0xc6, 0x3a]))
+    currenttime = NabuSegment()
+    sendBytes(currenttime.get_time_segment())
+
+# TODO:  We can probably get rid of handle_0xf0_request, handle_0x0f_request and handle_0x03_request
+# TODO:  as these bytes may have been from RS-422 buffer overruns / other errors
+
+def handle_0xf0_request(data):  
+    sendBytes(bytes([0xe4]))
+
+def handle_0x0f_request(data):
+    sendBytes(bytes([0xe4]))
+
+def handle_0x03_request(data):
+    sendBytes(bytes([0xe4]))
+
+def handle_reset_segment_handler(data):
+    sendBytes(bytes([0x10, 0x06, 0xe4]))
+
+def handle_reset(data):
+    send_ack()
+
+def handle_get_status(data):
+    global channelCode
+    send_ack()
+
+    response = recvBytes()
+    if channelCode is None:
+        print("* Channel Code is not set yet.")
+        # Ask NPC to set channel code
+        sendBytes(bytes([0x9f, 0x10, 0xe1]))
+    else:
+        print("* Channel code is set to " + channelCode)
+        # Report that channel code is already set
+        sendBytes(bytes([0x1f, 0x10, 0xe1]))
+
+def handle_set_status(data):
+    sendBytes(bytes([0x10, 0x06, 0xe4]))
+
+def handle_download_segment(data):
+    # ; pak load request
+    # [11]        NPC       $84
+    #              NA        $10 06
+    send_ack()
+    segmentNumber=recvBytesExactLen(1)[0]
+    pakNumber=bytes(reversed(recvBytesExactLen(3)))
+    pakId=str(pakNumber.hex())
+    print("* Requested Pak ID: " + pakId + "  * Requested Segment Number: " + str(segmentNumber))
+
+    if pakId == "7fffff":
+        print("Time packet requested")
+        sendBytes(bytes([0xe4, 0x91]))
+        pakId == ""
+    else:
+        sendBytes(bytes([0xe4, 0x91]))
+        response = recvBytes(2)
+        print("* Response from NPC: " + response.hex(" "))
+
+    # Get Segment from internal segment store
+        if pakId not in paks:
+            print("Pak not already in memory... loading...")
+            loadpak(pakId)
+            sendBytes(bytes([0x91]))
+        pak = paks[pakId]
+
+                
+    # Get requested segment from that pak
+        segment_data = pak.get_segment(segmentNumber)
+
+    # Dump information about segment.  'segment' is otherwise unused
+        segment = NabuSegment()
+        segment.ingest_bytes(segment_data)
+##            print("* Segment to send: " + segment_data.hex(' ')) 
+#            print("* Pak ID: "+ segment.pak_id.hex())
+#            print("* Segment Number: " + segment.segmentnum.hex())
+#            print("* Pak owner: " + segment.pak_owner.hex())
+#            print("* Pak tier: " + segment.pak_tier.hex())
+#            print("* Pak mystery_bytes: " + segment.pak_mystery_bytes.hex())
+#            print("* Segment Type: " + segment.segment_type.hex())
+#            print("* Segment Number: " + segment.segment_number.hex())
+#            print("* Segment Offset: " + segment.segment_offset.hex())
+#            print("* Segment CRC: " + segment.segment_crc.hex())
+#            print("* Segment length: {}".format(len(segment_data)))
+
+    # check checksum
+        seglength= len(segment_data)
+#            print(seglength)
+#            print(segment_data)
+        checkedpack=NabuSegment()
+        sd=bytearray(segment_data)
+        chk=checkedpack.add_checksum(sd[0:seglength-2])
+#            print(chk[-2:].hex(' '))
+#            print(bytes(chk[-2:]))
+#            print(bytes(segment.segment_crc))
+        if chk[-2:] == segment.segment_crc:
+            print("Pak: "+segment.pak_id.hex()+"  Segment: "+segment.segmentnum.hex()+ "  Checksum: "+segment.segment_crc.hex()+"    [Checksum Valid!]")
+        else:
+            print("Pak: "+segment.pak_id.hex()+"  Segment: "+segment.segmentnum.hex()+ "  Checksum: "+segment.segment_crc.hex())
+            print("##### Corrupt PAK file! #####  Checksum:", segment.segment_crc.hex(), "Should be:", chk[-2:].hex(), " ##### fixing...")
+            segment_data = chk
+                
+    # escape pack data (0x10 bytes should be escaped maybe?)
+        escaped_segment_data = escapeUploadBytes(segment_data)
+        sendBytes(escaped_segment_data)
+        sendBytes(bytes([0x10, 0xe1]))
+
+def handle_set_channel_code(data):
+    global channelCode
+    send_ack()
+    data = recvBytesExactLen(2)
+    while len(data) < 2:
+        remaining = 2 - len(data)
+        print("Waiting for channel code")
+        print(data.hex(' '))
+        data = data + recvBytes(remaining)
+
+    print("* Received Channel code bytes: " + data.hex())
+    channelCode = bytes(reversed(data)).hex()
+    print("* Channel code: " + channelCode)
+    sendBytes(bytes([0xe4]))
+
+def handle_0x8f_req(data):
+    print("* 0x8f request")
+    data = recvBytes()
+    sendBytes(bytes([0xe4]))
+
+def handle_unimplemented_req(data):
+    print("* ??? Unimplemented request")
+    print("* " + data.hex(' '))
+
+def escapeUploadBytes(data):
+    escapedBytes = bytearray()
+
+    for idx in range(len(data)):
+        byte=data[idx]
+        if(byte == 0x10):
+            escapedBytes.append(byte)
+            escapedBytes.append(byte)
+        else:
+            escapedBytes.append(byte)
+
+    return escapedBytes
+
+def sendBytes(data):
+    chunk_size=6
+    index=0
+    delay_secs=0
+    end=len(data)
+
+    while index + chunk_size < end:
+        ser.write(data[index:index+chunk_size])
+#        print("NA-->NPC:  " + data[index:index+chunk_size].hex(' '))
+        index += chunk_size
+        time.sleep(delay_secs)
+
+    if index != end:
+#        print("NA-->NPC:  " + data[index:end].hex(' '))
+        ser.write(data[index:end])
+
+def recvBytesExactLen(length=None):
+    if(length is None):
+        return None
+    data = recvBytes(length)
+    while len(data) < length:
+        remaining = length - len(data)
+#        print("Waiting for {} more bytes".format(length - len(data)))
+        print(data.hex(' '))
+        time.sleep(0.01)
+        data = data + recvBytes(remaining)
+    return data
+
+def recvBytes(length = None):
+    if(length is None):
+        data = ser.read(MAX_READ)
+    else:
+        data = ser.read(length)
+    if(len(data) > 0):
+        print("NPC-->NA:   " + data.hex(' '))
+    return data
+
+# Loads pak from file, assumes file names are all upper case with a lower case .pak extension
+
+def loadpak(filename):
+    file = filename.upper()
+    print("* Loading NABU Segments into memory")
+    pak1 = NabuPak()
+    if os.path.exists( args.paksource + file + ".pak") == False:
+        print("Pak file does not exist... here, have some penguins instead.")
+        file = "000120"
+    pak1.ingest_from_file( args.paksource + file + ".pak")
+    paks[filename] = pak1
+
+######  Begin main code here
+global paks
+paks = {}    # Creates library variable to store loaded paks in memory
+
+MAX_READ=65535
+DEFAULT_BAUDRATE=111863
+DEFAULT_SERIAL_PORT="/dev/ttyUSB0"
+DEFAULT_PAK_DIRECTORY="./cycle2pak/"
+
+# channelCode = None
+channelCode = '0000'
+
+
+
+parser = argparse.ArgumentParser()
+# Optional serial port selection
+parser.add_argument("-t", "--ttyname",
+help="Set serial device (e.g. /dev/ttyUSB0)",
+default=DEFAULT_SERIAL_PORT)
+# Optional baud rate selection
+parser.add_argument("-b", "--baudrate",
+type=int,
+help="Set serial baud rate (default: {} BPS)".format(DEFAULT_BAUDRATE),
+default=DEFAULT_BAUDRATE)
+# Optional pak directory selection
+parser.add_argument("-p", "--paksource",
+help="Set location of the pak files (default: {} )".format(DEFAULT_PAK_DIRECTORY),
+default=DEFAULT_PAK_DIRECTORY) 
+		
+args = parser.parse_args()
+
+loadpak("000001")
+
+# Some hard-coded things here (timeout, stopbits)
+ser = serial.Serial(port=args.ttyname, baudrate=args.baudrate, timeout=0.5, stopbits=serial.STOPBITS_TWO)
+
+
+# Some hard-coded things here. 
+#ser = serial.Serial(port='/dev/ttyUSB0', baudrate=111865, timeout=0.5, stopbits=serial.STOPBITS_TWO)
+
+while True:
+    data = recvBytes()
+    if len(data) > 0:
+        req_type = data[0]
+
+        if req_type == 0x03:
+            print("* 0x03 request")
+            handle_0x03_request(data)
+        elif req_type == 0x0f:
+            print("* 0x0f request")
+            handle_0x0f_request(data)
+        elif req_type == 0xf0:
+            print("* 0xf0 request")
+            handle_0xf0_request(data)
+        elif req_type == 0x80:
+            print("* Reset segment handler")
+            handle_reset_segment_handler(data)
+        elif req_type == 0x81:
+            print("* Reset")
+            handle_reset(data)
+        elif req_type == 0x82:
+            print("* Get Status")
+            handle_get_status(data)
+        elif req_type == 0x83:
+            print("* Set Status")
+            handle_set_status(data)
+        elif req_type == 0x84:
+            print("* Download Segment Request")
+            handle_download_segment(data)
+        elif req_type == 0x85:
+            print("* Set Channel Code")
+            handle_set_channel_code(data)
+            print("* Channel code is now " + channelCode)
+        elif req_type == 0x8f:
+            print("* Handle 0x8f")
+            handle_0x8f_req(data)
+        elif req_type == 0x10:
+            print("got request type 10, sending time")
+            send_time()
+            sendBytes(bytes([0x10, 0xe1]))
+
+        else:
+            print("* Req type {} is Unimplemented :(".format(data[0]))
+            handle_unimplemented_req(data)
+
---
+++ nabu_data.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python3
+#
+# Original code: NABU Adaptor Emulator - Copyright Mike Debreceni - 2022
+#       https://github.com/mdebreceni/nabu-pc-playground/
+#
+# Expanded and modified by Sark, 12/28/2022
+#   Terminology fixup to update variable names, notes and messages to match other conventions:
+#       Segment is a chunk of a program, less than 1K in size
+#       Pak is a group of segments
+#   Added CRC generation and time segment creation to NabuSegment class
+#
+# 
+# A NABU program (called a pak) is a single file of concatenated binaries called segments:
+#
+# pak
+#         +-----------+-----------+-----------+-----------+
+#         | segment 0 | segment 1 | segment...| segment n |
+#         +-----------+-----------+-----------+-----------+
+#         
+# Each segment is no more than 1KB and has this structure
+# 
+# pack
+#         +----------------+
+#         | header 18 bytes|
+#         +----------------+
+#         | code =<1004    |
+#         | bytes          |
+#         +----------------+
+#         | CRC  2 bytes   |
+#         +----------------+
+#         
+# The meaning of header bytes is as follows:
+#         bytes 0,1:     segment length (excluding the header and CRC bytes, lsb first)
+#         bytes 2,3,4:   pak id (msb firs)
+#         byte 5:        segment id (with the first id = 0)
+#         byte 6:        pak owner (the same for all PAKs; in YUNN segment owner=1)
+#         bytes 7-10:    pak tier  (the same for all PAKs; in YUNN, tier= $08 $00 $00 $02)
+#         bytes 11,12:   mystery bytes (the same for all PAKs; in YUNN, these bytes are $80 $19)
+#         byte  13:      segment type
+#         byte  14,15:   segment number (the same as byte 5)
+#         byte  16,17:   offset -- pointer to the beginning of the segment from the beginning of the pak
+# 
+
+import datetime
+
+class NabuPak:
+    def __init__(self):
+        self.segments = {}
+
+    def load_segment(self, segment_bytes, segment_id):
+        self.segments[segment_id] = segment_bytes
+
+    def get_segment(self, segment_id):
+#        print("* get_segment(segment_id = {})".format(segment_id))
+        if segment_id in self.segments:
+#            print("* Found!")
+            return self.segments[segment_id]
+        else:
+            print("* Not found :(")
+            return None
+
+    def get_segment_count(self):
+        return len(self.segments)
+
+    def parse_pak(self, pak_bytes):
+        index = 0
+        junkbytes = 0
+        endflag = len(pak_bytes) - 1
+        while pak_bytes[endflag] == 0x1a:
+        	endflag = endflag - 1
+        	junkbytes = junkbytes + 1	
+        if junkbytes != 0:
+        	print(" ##### Found ", junkbytes, "extra 1a's at the end of pak file. Trimming.")
+        while index < endflag + 1 :
+            segment_length = pak_bytes[index] + pak_bytes[index + 1] * 256
+            segment_id = pak_bytes[index + 5]
+
+#            print("* Segment length is {}".format(segment_length))
+#            print("* Segment ID is {}".format(segment_id))
+            index += 2
+            segment_end = index + segment_length
+#            print("* Index = {}.  Segment_end = {}.".format(index, segment_end))
+            print("Segment ID: ", segment_id, " Index: ", index, " Segment end: ", segment_end, " Length: ", segment_length, "[",hex(segment_length),"]")
+            segment_bytes = pak_bytes[index:segment_end]
+##            print("* Segment bytes: {}".format(segment_bytes.hex(' ')))
+
+            self.segments[segment_id] = segment_bytes
+            index += segment_length
+
+    def ingest_from_file(self, pakfile):
+        f = open(pakfile, "rb")
+        contents = bytes(f.read())
+        print(" * Ingesting Segments from {}:".format(pakfile) + contents.hex(' '))
+        self.parse_pak(contents)
+        
+class NabuSegment:
+    def __init__(self):
+        pak_pak_id = None
+        pak_segmentnum = None
+        pak_owner = None
+        pak_tier = None
+        pak_mystery_bytes = None
+        segment_type = None
+        segment_number = None
+        segment_offset = None
+        segment_crc = None
+        segment_bytes = None
+
+    def ingest_bytes(self, segment_bytes):
+        self.pak_id = bytes(segment_bytes[0:3])
+        self.segmentnum = bytes(segment_bytes[3:4])
+        self.pak_owner = bytes(segment_bytes[4:5])
+        self.pak_tier = bytes(segment_bytes[5:9])
+        self.pak_mystery_bytes = bytes(segment_bytes[9:11])
+        self.segment_type = bytes(segment_bytes[11:12])
+        self.segment_number = bytes(segment_bytes[12:14])
+        self.segment_offset = bytes(segment_bytes[14:16])
+        self.segment_crc = bytes(segment_bytes[-2:])
+        self.segment_bytes = segment_bytes
+
+    def __init__(self):
+        self.crctable=[0x0000,0x1021,0x2042,0x3063,0x4084,0x50a5,0x60c6,0x70e7,0x8108,0x9129,0xa14a,0xb16b,0xc18c,0xd1ad,0xe1ce,0xf1ef,0x1231,0x0210,0x3273,0x2252,0x52b5,0x4294,0x72f7,0x62d6,0x9339,0x8318,0xb37b,0xa35a,0xd3bd,0xc39c,0xf3ff,0xe3de,0x2462,0x3443,0x0420,0x1401,0x64e6,0x74c7,0x44a4,0x5485,0xa56a,0xb54b,0x8528,0x9509,0xe5ee,0xf5cf,0xc5ac,0xd58d,0x3653,0x2672,0x1611,0x0630,0x76d7,0x66f6,0x5695,0x46b4,0xb75b,0xa77a,0x9719,0x8738,0xf7df,0xe7fe,0xd79d,0xc7bc,0x48c4,0x58e5,0x6886,0x78a7,0x0840,0x1861,0x2802,0x3823,0xc9cc,0xd9ed,0xe98e,0xf9af,0x8948,0x9969,0xa90a,0xb92b,0x5af5,0x4ad4,0x7ab7,0x6a96,0x1a71,0x0a50,0x3a33,0x2a12,0xdbfd,0xcbdc,0xfbbf,0xeb9e,0x9b79,0x8b58,0xbb3b,0xab1a,0x6ca6,0x7c87,0x4ce4,0x5cc5,0x2c22,0x3c03,0x0c60,0x1c41,0xedae,0xfd8f,0xcdec,0xddcd,0xad2a,0xbd0b,0x8d68,0x9d49,0x7e97,0x6eb6,0x5ed5,0x4ef4,0x3e13,0x2e32,0x1e51,0x0e70,0xff9f,0xefbe,0xdfdd,0xcffc,0xbf1b,0xaf3a,0x9f59,0x8f78,0x9188,0x81a9,0xb1ca,0xa1eb,0xd10c,0xc12d,0xf14e,0xe16f,0x1080,0x00a1,0x30c2,0x20e3,0x5004,0x4025,0x7046,0x6067,0x83b9,0x9398,0xa3fb,0xb3da,0xc33d,0xd31c,0xe37f,0xf35e,0x02b1,0x1290,0x22f3,0x32d2,0x4235,0x5214,0x6277,0x7256,0xb5ea,0xa5cb,0x95a8,0x8589,0xf56e,0xe54f,0xd52c,0xc50d,0x34e2,0x24c3,0x14a0,0x0481,0x7466,0x6447,0x5424,0x4405,0xa7db,0xb7fa,0x8799,0x97b8,0xe75f,0xf77e,0xc71d,0xd73c,0x26d3,0x36f2,0x0691,0x16b0,0x6657,0x7676,0x4615,0x5634,0xd94c,0xc96d,0xf90e,0xe92f,0x99c8,0x89e9,0xb98a,0xa9ab,0x5844,0x4865,0x7806,0x6827,0x18c0,0x08e1,0x3882,0x28a3,0xcb7d,0xdb5c,0xeb3f,0xfb1e,0x8bf9,0x9bd8,0xabbb,0xbb9a,0x4a75,0x5a54,0x6a37,0x7a16,0x0af1,0x1ad0,0x2ab3,0x3a92,0xfd2e,0xed0f,0xdd6c,0xcd4d,0xbdaa,0xad8b,0x9de8,0x8dc9,0x7c26,0x6c07,0x5c64,0x4c45,0x3ca2,0x2c83,0x1ce0,0x0cc1,0xef1f,0xff3e,0xcf5d,0xdf7c,0xaf9b,0xbfba,0x8fd9,0x9ff8,0x6e17,0x7e36,0x4e55,0x5e74,0x2e93,0x3eb2,0x0ed1,0x1ef0]
+
+    def get_time_segment(self):
+        now = datetime.datetime.now()
+        time_bytes=bytearray([])
+        time_bytes[0:3] = [127, 255, 255] # segment ID = 0x7fffff
+        time_bytes[3:4] = [0] # packet number = 0x00
+        time_bytes[4:5] = [1] # owner = 0x01
+        time_bytes[5:9] = [127, 255, 255, 255] # tier = 0x7fffffff
+        time_bytes[9:11] = [127, 128] # mystery byte = 0x7f80
+        time_bytes[11:12] = [32] # pack type = 0x20 (16 for 0x10)
+        time_bytes[12:14] = [0, 0] # packet number zero
+        time_bytes[14:16] = [0, 0] # offset zero
+        time_bytes[16:18] = [2, 2] ##, 84, 1, 1, 0, 40, 0]
+        time_bytes[18:19] = [(now.weekday()+2)%7]
+        time_bytes[19:20] = [83] # 1983
+        time_bytes[20:21] = [now.month]
+        time_bytes[21:22] = [now.day]
+        time_bytes[22:23] = [now.hour]
+        time_bytes[23:24] = [now.minute]
+        time_bytes[24:25] = [now.second] 
+#        self.timeseg = time_bytes[0:25]
+        timeseg = self.add_checksum(time_bytes[0:25])
+        return timeseg
+		
+    def add_checksum(self, segment_data):
+        seglength = len(segment_data)
+        crc=65535
+        for z in segment_data:
+#            print("byte", z)
+            checksum = (crc >> 8 ^ z)
+            checksum = hex(checksum & 0xff)
+            checksum = int(checksum, 0)
+            crc <<= 8
+            crc = int(hex(crc & 0xffff),0)
+            crc ^= self.crctable[checksum]
+            crc = int(hex(crc & 0xffff),0)
+        crcbyte1=(int(int(hex(crc & 0xff00),0)/256))^ 0xff
+#        print("crcbyte1", crcbyte1, hex(crcbyte1))
+        crcbyte2=(int(hex(crc & 0x00ff),0))^ 0xff
+#        print("crcbyte2", crcbyte2, hex(crcbyte2))
+        segment_data[seglength:seglength+2] = bytes([crcbyte1,crcbyte2])
+        return segment_data
+        
