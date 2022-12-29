781b3e88f662bc987f3d97f8d21eff21b2a374d7
Sark2338 <121634424+Sark2338@users.noreply.github.com>
2022-12-29 18:04:38 +0000

    Delete homebrew-code directory

--- homebrew-code/Mandelbrot/src/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-PAK_DIR=~/code/nabu-homebrew/compiled-pak
-
-zcc +z80 -mz80 -startup 0 -zorg 0x140D --no-crt -lm Mandelbrot.c -O2 -o 000001.nabu && 
-	mv 000001_code_compiler.bin 000001.nabu && 
-	mv 000001.nabu $PAK_DIR
-
-
