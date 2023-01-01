4e21d94f4fb380ff6d33e9e4c0cecf70bc2227ee
j__r0d <jkozeal@gmail.com>
2022-12-31 23:30:43 -0700

    adjust logging format

--- logger.py
+++ logger.py
@@ -1,3 +1,3 @@
 import logging
 
-logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s[%(levelname)-7s]: %(message)s', level=logging.INFO)
